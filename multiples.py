67c3182f360ac8996ea9218b3181145986596492
Henry Vu <henryvu25@gmail.com>
2020-03-14 21:35:06 -0700

    Update notes for multiples.py
    added notes to explain the different functions and operators in the program

--- multiples.py
+++ multiples.py
@@ -1,7 +1,7 @@
-def is_multiple(n, m):
-    if n % m == 0:
+def is_multiple(n, m): #defined the function with two arguments
+    if n % m == 0: #modulo to see if n is a multiple of m. so if n is divided by m, will the remainder be 0?
         return True
     else:
-        return False
+        return False #if the remainer is anything but 0, the function will return False
     
-print(is_multiple(24, 4))
+print(is_multiple(24, 4)) #this is a test inputting arguments 24 and 4 which will print the returned value True
